f91d77d13b55cc4c23ebffbe9c97ebf1420aecbb
Stanislavcod <stas.dryk@mail.ru>
2023-01-09 15:31:27 +0300

    create Database model

---
+++ StudentAccounting.Model/DatabaseContext/ApplicationDatabaseContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentAccountin.Model.DatabaseModels;
+using StudentAccounting.Model.DataBaseModels;
+
+namespace StudentAccounting.Model
+{
+    public class ApplicationDatabaseContext : DbContext
+    {
+        public ApplicationDatabaseContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder _context)
+        {
+            _context.UseSqlServer("Server=STASVCODE\\SQLEXPRESS;DataBase=StudentAccountingDatabase;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+        public DbSet<ApplicationsInTheProject> ApplicationsInTheProjects { get; set; }
+        public DbSet<Employment> Employments { get; set; }
+        public DbSet<FinalProject> FinalProjects { get; set; }
+        public DbSet<Regulation> Regulations { get; set; }
+        public DbSet<StagesOfProject> StagesOfProjects { get; set; }
+        public DbSet<TrainingCourses> TrainingCourses { get; set; }
+        public DbSet<Vacancy> Vacancies { get; set; }
+        public DbSet<Bonus> Bonuses { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Individuals> Individuals { get; set; }
+        public DbSet<Organization> Organizations { get; set; }
+        public DbSet<Participants> Participants { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Rang> Rangs { get; set; }
+        public DbSet<Student> Students {get; set;}
+    }
+}
