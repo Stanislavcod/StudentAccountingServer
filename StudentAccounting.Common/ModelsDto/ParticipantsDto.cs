2c8c815c7f55cfe1ae1f93e6ab6acaf40cd16634
Stanislavcod <stas.dryk@mail.ru>
2023-01-16 16:07:59 +0300

    update DTOMOdels

--- StudentAccounting.Common/ModelsDto/ParticipantsDto.cs
+++ StudentAccounting.Common/ModelsDto/ParticipantsDto.cs
@@ -7,17 +7,16 @@ namespace StudentAccounting.Common.ModelsDto
     public class ParticipantsDto
     {
         public int Id { get; set; }
-        public DateTime DateEntry { get; set; }
-        public DateTime DateExit { get; set; }
-        public string Status { get; set; } = string.Empty;
-        public string GitHub { get; set; } = string.Empty;
         public int IndividualsId { get; set; }
         public Individuals? Individuals { get; set; }
-        public int? RangId { get; set; }
-        public Rang? Rang { get; set; }
         public int UserId { get; set; }
         public User? User { get; set; }
-        public List<ApplicationsInTheProject> ApplicationsInTheProjects { get; set; } = new();
-        public List<Employment> Employments { get; set; } = new();
+        public DateTime DateEntry { get; set; }
+        public DateTime? DateExit { get; set; }
+        public int mmr { get; set; }
+        public string? Status { get; set; } = string.Empty;
+        public string? GitHub { get; set; } = string.Empty;
+        public List<ApplicationsInTheProject>? ApplicationsInTheProjects { get; set; } = new();
+        public List<Employment>? Employments { get; set; } = new();
     }
 }
