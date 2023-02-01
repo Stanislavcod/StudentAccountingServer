75f805d036c07acd41916ff94dc0bd65cd4b720b
Stanislavcod <stas.dryk@mail.ru>
2023-02-01 22:15:07 +0300

    add include in Employment

--- SyudentAccounting.BusinessLogic/Services/Implementations/EmploymentService.cs
+++ SyudentAccounting.BusinessLogic/Services/Implementations/EmploymentService.cs
@@ -20,7 +20,7 @@ namespace StudentAccounting.BusinessLogic.Services.Implementations
         }
         public IEnumerable<Employment> Get()
         {
-            return _context.Employments.Include(x => x.Position).Include(x => x.Participants).Include(x=> x.FinalProjects).AsNoTracking().ToList();
+            return _context.Employments.Include(x => x.Position).Include(x => x.Participants).Include(x=> x.Participants.Individuals).Include(x=> x.FinalProjects).AsNoTracking().ToList();
         }
         public Employment Get(int id)
         {
