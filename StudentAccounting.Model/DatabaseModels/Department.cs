8c75279fbd1acc2efefc82844c0d67f9c62060a9
Stanislavcod <stas.dryk@mail.ru>
2023-02-08 00:24:32 +0300

    test

--- StudentAccounting.Model/DatabaseModels/Department.cs
+++ StudentAccounting.Model/DatabaseModels/Department.cs
@@ -9,7 +9,8 @@ namespace StudentAccounting.Model.DataBaseModels
         public string FullName { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public DateTime DateStart { get; set; }
-        public DateTime DateEnd { get; set; }
+        public DateTime? DateEnd { get; set; }
+        public int? DirectorId { get; set; }
         public string Status { get; set; } = string.Empty;
         public List<Position>? Positions { get; set; } = new();
         public int OrganizationId { get; set; }
