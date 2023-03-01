1b19d879d3c20b122d0ccd9baa4b78ec47d1e460
egor <egor555.novik@gmail.com>
2023-03-01 23:03:51 +0300

    edited AuthService.cs and AuthController.cs

--- StudentAccounting/Controllers/AuthController.cs
+++ StudentAccounting/Controllers/AuthController.cs
@@ -9,27 +9,42 @@ namespace StudentAccounting.Controllers
     public class AuthController : Controller
     {
         private readonly IAuthService _authService;
+        
         public AuthController(IAuthService authService)
         {
             _authService = authService;
         }
+        
         [HttpGet("RegisterAdmin")]
         public IActionResult RegisterAdmin()
         {
-            return Ok(_authService.RegisterAdmin());
+            var response = _authService.RegisterAdmin();
+            
+            return Ok(response);
         }
+        
         [HttpPost("Login")]
         public IActionResult Login(LoginDTO loginDto)
         {
-            return Ok(_authService.Login(loginDto));
+            var response = _authService.Login(loginDto);
+            
+            return Ok(response);
         }
+        
         [Authorize(Roles = "Admin,DirectorOrganizational")]
         [HttpPost("Register")]
         public IActionResult Register(RegisterDto registerModel)
         {
             if (_authService.UserExists(registerModel.Login))
+            {
                 return BadRequest("UserName Is Already Taken");
-            if (!_authService.Register(registerModel)) return BadRequest();
+            }
+
+            if (!_authService.Register(registerModel))
+            {
+                return BadRequest();
+            }
+            
             return Ok("user was registered");
         }
 
@@ -37,7 +52,9 @@ namespace StudentAccounting.Controllers
         [HttpPut]
         public IActionResult Put([FromBody] RefreshTokenDto model)
         {
-            return Ok(_authService.Refresh(model));
+            var response = _authService.Refresh(model);
+            
+            return Ok(response);
         }
     }
 }
