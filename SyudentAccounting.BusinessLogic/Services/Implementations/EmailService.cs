6c65e157162630863c5e8d90664d4f630e1dd86c
egor <egor555.novik@gmail.com>
2023-03-03 17:33:06 +0300

    edited EmailService.cs

--- SyudentAccounting.BusinessLogic/Services/Implementations/EmailService.cs
+++ SyudentAccounting.BusinessLogic/Services/Implementations/EmailService.cs
@@ -1,16 +1,29 @@
 ﻿using MimeKit;
 using StudentAccounting.BusinessLogic.Services.Contracts;
 using MailKit.Net.Smtp;
+using Microsoft.Extensions.Configuration;
 
 namespace StudentAccounting.BusinessLogic.Services.Implementations
 {
     public class EmailService : IEmailService
     {
+        private const string Name = "PolessUp";
+        private const string Host = "smtp.gmail.com";
+        private const int Port = 465;
+        private readonly string _emailAddress;
+        private readonly string _emailPassword;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _emailAddress = configuration["Email:Address"];
+            _emailPassword = configuration["Email:Password"];
+        }
+        
         public void SendEmailMessage(string email, string subject, string message)
         {
             var emailMessage = new MimeMessage();
 
-            emailMessage.From.Add(new MailboxAddress("PolessUp", "polessup@gmail.com"));
+            emailMessage.From.Add(new MailboxAddress(Name, _emailAddress));
 
             emailMessage.To.Add(new MailboxAddress("", email));
             emailMessage.Subject = subject;
@@ -21,11 +34,12 @@ namespace StudentAccounting.BusinessLogic.Services.Implementations
                     
             using (SmtpClient client = new SmtpClient())
             {
-                client.Connect("smtp.gmail.com", 465, true);
+                client.Connect(Host, Port, true);
 
-                client.Authenticate("polessup@gmail.com", "xdskhxhcbwycmaut");
+                client.Authenticate(_emailAddress, _emailPassword);
 
                 client.Send(emailMessage);
+                
                 client.Disconnect(true);
             }
         }
